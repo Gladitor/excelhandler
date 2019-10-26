992f6fda408a0ad04df9d86119267ebe7cc86d00
JIAY <jying1989@163.com>
2019-10-26 09:13:21 +0800

    水平合并所有sheet到1个文件的1个sheet

---
+++ src/DataTableHelper.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelHandler
+{
+    public class DataTableHelper
+    {
+        /// <summary>
+        /// 将两个列不同的DataTable合并成一个新的DataTable
+        /// </summary>
+        /// <param name="dt1">表1</param>
+        /// <param name="dt2">表2</param>
+        /// <param name="DTName">合并后新的表名</param>
+        /// <returns></returns>
+        public static DataTable UniteDataTable(DataTable dt1, DataTable dt2, string DTName)
+        {
+            DataTable dt3 = dt1.Clone();
+            for (int i = 0; i < dt2.Columns.Count; i++)
+            {
+                string colname = getColumnName(dt3, dt2.Columns[i].ColumnName);
+                dt3.Columns.Add(colname);
+            }
+            object[] obj = new object[dt3.Columns.Count];
+
+            for (int i = 0; i < dt1.Rows.Count; i++)
+            {
+                dt1.Rows[i].ItemArray.CopyTo(obj, 0);
+                dt3.Rows.Add(obj);
+            }
+
+            if (dt1.Rows.Count >= dt2.Rows.Count)
+            {
+                for (int i = 0; i < dt2.Rows.Count; i++)
+                {
+                    for (int j = 0; j < dt2.Columns.Count; j++)
+                    {
+                        dt3.Rows[i][j + dt1.Columns.Count] = dt2.Rows[i][j].ToString();
+                    }
+                }
+            }
+            else
+            {
+                DataRow dr3;
+                for (int i = 0; i < dt2.Rows.Count - dt1.Rows.Count; i++)
+                {
+                    dr3 = dt3.NewRow();
+                    dt3.Rows.Add(dr3);
+                }
+                for (int i = 0; i < dt2.Rows.Count; i++)
+                {
+                    for (int j = 0; j < dt2.Columns.Count; j++)
+                    {
+                        dt3.Rows[i][j + dt1.Columns.Count] = dt2.Rows[i][j].ToString();
+                    }
+                }
+            }
+            dt3.TableName = DTName;
+            return dt3;
+        }
+
+        static string getColumnName(DataTable dt,string columnName)
+        {
+            while(dt.Columns.Contains(columnName))
+            {
+                columnName += "(repeat)"; 
+            }
+            return columnName;
+        }
+
+    }
+}
